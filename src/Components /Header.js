66efd8fbd33e0baaaad438e7cf9a766d1712c4ae
Kortney <kortney@iMac.local>
2022-08-16 16:25:48 -0300

    wrote component for header

---
+++ src/Components /Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header id="header">
+            <span className="logo">
+                <img src="" alt=""></img>
+            </span>
+            <h1>Kortney Foley</h1>
+        </header>
+    );
+}
