bdcea4881f3868167793e8b350e2edd0672e00b3
Kortney <kortney@iMac.local>
2022-08-18 16:07:09 -0300

    Did CSS styling to center the title and added horizontial rule. Gave li's height width and background colour to create a color coded table.

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -3,39 +3,39 @@ import "./Skills.css";
 
 export default function Skills() {
     return (
-        <section id="skills" className="sectionTop">
-            <header className="major">
-                <h2>Skills</h2>
+        <section id="skills" className="skillsSection">
+            <header className="skillsHeader">
+                <h1>Skills</h1>
                 <p></p>
+                <hr className="horizontalRule"></hr>
             </header>
-            <ul className="statistics">
-                <li>
+            <ul className="skills">
+                <li className="styleA">
                     <span></span>
                     <strong>HTML</strong>
                 </li>
-                <li>
+                <li className="styleB">
                     <span></span>
                     <strong>CSS</strong>
                 </li>
-                <li>
+                <li className="styleC">
                     <span></span>
                     <strong>Javascript</strong>
                 </li>
-                <li>
+                <li className="styleD">
                     <span></span>
                     <strong>React</strong>
                 </li>
-                <li>
-                    <span>Python</span>
-                    <strong></strong>
+                <li className="styleE">
+                    <span></span>
+                    <strong>Python</strong>
                 </li>
             </ul>
             <p className="content">
-                {" "}
                 I started by making a few basic projects in python. From there I
                 delevoped my knowledge of HTML, CSS and Javascript. More
                 recently I've been focusing on building apps in React, focusing
-                on those that manipulate data from APIs.{" "}
+                on those that manipulate data from APIs.
             </p>
         </section>
     );
